95b420d55389daf8421f8e38251d9532d02c6a14
Dan Drinkard <dan.drinkard@gmail.com>
2014-01-24 22:19:49 -0500

    Add share counts for facebook

--- extensions/socialite.facebook-share.js
+++ extensions/socialite.facebook-share.js
@@ -15,7 +15,7 @@
      *
      */
 
-    function addEvent(obj, evt, fn, capture) {
+    var addEvent = function(obj, evt, fn, capture) {
         if (window.attachEvent) {
             obj.attachEvent("on" + evt, fn);
         }
@@ -23,6 +23,49 @@
             if (!capture) capture = false; // capture
             obj.addEventListener(evt, fn, capture);
         }
+    },
+
+    facebookGetCount = function(instance, options) {
+        var params = getParams(instance),
+            counturl = params['counturl'] || params['url'],
+            callback = 'socialiteFacebookShare' + instance.uid,
+            endpoint = "https://graph.facebook.com/fql?q=" + encodeURIComponent("select total_count from link_stat where url = '") + counturl + "'&callback=" + callback,
+            options = options || {},
+            scriptTag = document.createElement('script');
+            scriptTag.src = endpoint;
+            document.getElementsByTagName('script')[0].insertBefore(scriptTag);
+            window[callback] = function(data){
+                instance.el.querySelectorAll('.counter')[0].innerHTML = data.data[0].total_count;
+                delete window[callback];
+                scriptTag.parentNode.removeChild(scriptTag);
+            }
+    },
+
+    getParams = function(instance) {
+        return parseQS(Socialite.getDataAttributes(instance.el, true));
+    },
+
+    parseQS = function(qs) {
+        var hsh = {},
+            parts = qs.split(/[&=]/);
+        for(var i in parts) {
+
+            if  (i%2) {
+                hsh[parts[i-1]] = decodeURIComponent(parts[i]);
+            };
+        };
+        return hsh;
+    },
+
+    appendStyleSheet = function() {
+        (document.querySelectorAll('style.custom-count-widgets').length || (function(){
+            var styleTag = document.createElement('style');
+            styleTag.className = 'custom-count-widgets';
+            styleTag.innerHTML = '.socialite .counter {position:relative; margin-left: 5px; vertical-align: middle; display:inline-block; min-width:6px; border:1px solid #e6e5e3; padding:6px 7px; font-family:"helvetica neue", helvetica, arial, sans-serif; font-size:13px; line-height:1.2; color:#222; text-decoration: none; /*border-radius: 2px;*/ }' +
+                                 '.socialite .counter:before { content:""; position:absolute; left: -5px; top: 10px; background:#fff; border:1px solid #e6e5e3; border-right:none; border-top:none; display:block; width:7px; height:6px; -webkit-transform: rotate(61deg) skewX(35deg); -moz-transform: rotate(61deg) skewX(35deg); -o-transform: rotate(61deg) skewX(35deg); -ms-transform: rotate(61deg) skewX(35deg); transform: rotate(61deg) skewX(35deg); }' +
+                                 '.socialite img { vertical-align: middle; }';
+            document.getElementsByTagName('head')[0].appendChild(styleTag);
+        })());
     }
 
     Socialite.widget('facebook', 'share', {
@@ -43,6 +86,12 @@
                 imgTag.src = instance.el.getAttribute('data-image');
                 el.appendChild(imgTag);
             }
+            if (instance.el.getAttribute('data-show-counts')) {
+                var counterTag = document.createElement('span')
+                counterTag.className = 'counter';
+                counterTag.innerHTML = '&hellip;';
+                el.appendChild(counterTag);
+            }
             addEvent(el, 'click', function(e){
                 var t = e? e.target : window.event.srcElement;
                 e.preventDefault();
@@ -50,7 +99,12 @@
             });
             instance.el.appendChild(el);
         },
-        activate: function(){}
+        activate: function(instance){
+            if (getParams(instance)['show-counts']) {
+                appendStyleSheet();
+                facebookGetCount(instance);
+            }
+        }
     });
 
 })(window, window.document, window.Socialite);
